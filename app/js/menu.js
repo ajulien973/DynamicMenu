42f14e9010da0aa92f421f041587c13262df407b
ekanseht <thesnake973@gmail.com>
2015-04-12 14:54:45 +0200

    added menu builder, init and view

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,4 +1,10 @@
-function MenuMaker() {
+function Node(id, name, children) {
+	this.id = id;
+	this.name = name;
+	this.children = children;
+}
+
+function MenuMaker(options) {
 	this.source = {
 		"lastModified" : 1428767401000,
 		"name" : "simple menu",
@@ -60,11 +66,48 @@ function MenuMaker() {
 				"children" : []
 			}]
 	};
+	this.children = [];
+	this.view = options.view;
+}
+
+MenuMaker.prototype = {
+	init : function() {
+		var nodes = this.source.nodes;
+		for(var i = 0; i < nodes.length; i++) {
+			var node = new Node(nodes[i].id, nodes[i].name, nodes[i].children);
+			this.view.addNode(node, this.view.getEl());
+		}
+	}
+}
+
+MenuMaker.View = function(options) {
+	//this.options = options;
+	this.options = {
+		el : "menu"
+	}
+}
+
+MenuMaker.View.prototype = {
+	getEl : function() {
+		return document.getElementById(this.options.el);
+	},
+	addNode : function(node, el) {
+		var li = document.createElement("LI");
+		var text = document.createTextNode(node.name);
+		li.appendChild(text); 
+		if(node.children.length > 0) {
+			var ul = document.createElement("UL");
+			for(var i = 0; i < node.children.length; i++) {
+				this.addNode(node.children[i], ul);
+			}
+			li.appendChild(ul);
+		}
+		el.appendChild(li);
+	}
 }
 
 window.onload = function(){
 	this.id = "menu";
-	this.menu = new MenuMaker();
-	console.dir(this.menu.source);
-	alert(id);
+	this.menu = new MenuMaker({view : new MenuMaker.View({})});
+	this.menu.init();
 }
